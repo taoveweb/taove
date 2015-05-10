762afcfdaffd1692f2ffb1fe5afbd6218a1ddb8b
taove <taoveweb@gmail.com>
2015-05-10 19:53:13 +0800

    add

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname + 'files'));
-app.use(express.static(__dirname + 'uploads'));
+app.use(express.static(path.join(__dirname, 'uploads')));
+
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/admin.js
+++ routes/admin.js
@@ -12,8 +12,9 @@ var ObjectId = db.ObjectId;
 
 router.get('/', getAlbum);
 router.get('/:projectTitle', getAlbum);//查
-router.delete('/', deleteAlbum);//册
 router.post('/', postAlbum);
+router.put('/', putAlbum);//册
+router.delete('/', deleteAlbum);//册
 
 router.get('/user', getUser);
 router.get('/photographer', getPhotographer);
@@ -26,7 +27,8 @@ function getAlbum(req, res, next) {
     if (!!req.params.projectTitle) {
         query = {projectTitle: req.params.projectTitle};
     }
-    albums.find(query, null, {skip: 10}, function (err, list) {
+    console.log(query);
+    albums.find(query, null, function (err, list) {
         if (err) {
             console.log(err);
             res.render('admin/album', {
@@ -61,15 +63,34 @@ function deleteAlbum(req, res, next) {
     });
 
 }
+
+function putAlbum(req,res,next){
+    console.log('putAlbum------------');
+    res.send('修改');
+}
 function postAlbum(req, res, next) {
 
     var form = new formidable.IncomingForm();
-    form.uploadDir = "./uploads";
+    form.uploadDir = "./uploads/images/";
     form.encoding = 'utf-8';
     form.keepExtensions = true;
+    form.multiples=true;
     form.parse(req, function (err, fields, files) {
+        console.log(files.img.length);
+
         var param = fields;
         var img = files.img;
+        var imgS=[];
+        for(var i=0;i<files.img.length;i++){
+            var imgItem=img[i];
+            imgS.push({
+                img_id: new ObjectId,
+                path: imgItem.path.replace("uploads\\", ""),
+                name: imgItem.path.replace("uploads\\", ""),
+                collectUser: [],
+                comment: []
+            })
+        }
         albums.create({
             projectTitle: param.projectTitle,
             photographer: param.photographer,
@@ -77,22 +98,14 @@ function postAlbum(req, res, next) {
             description: param.description,
             area: param.area,
             style: param.style,
-            img: [
-                {
-                    img_id: new ObjectId,
-                    path: img.path.replace("uploads\\", ""),
-                    name: img.path.replace("uploads\\", ""),
-                    collectUser: [],
-                    comment: []
-                }
-            ],
+            img: imgS,
             customer: param.customer,
             createdBy: param.createdBy,
             modifiedOn: new Date().getTime(),
-            photoOn: param.photoOn
+            photoOn:  new Date().getTime()
 
         }, function (err, list) {
-            if (err)   res.send('失败了');
+            if (err)   res.send('失败了'+err);
             res.send('成功了')
         });
         //res.end(util.inspect({fields: fields, files: files}));

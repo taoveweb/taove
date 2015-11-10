8ab56c04428c2b3d982cae52e28a89e6652ce413
taove <taoveweb@gmail.com>
2015-11-10 19:04:16 +0800

    add

--- routes/pc/package.js
+++ routes/pc/package.js
@@ -9,15 +9,15 @@ router.get('/d', index_d);
 router.get('/e', index_e);
 
 function index_b(req, res, next) {
-    res.render('pc/packageb', { title: '桃微婚纱摄影--B套系',layout:'layout_pc' });
+    res.render('pc/package/packageb', { title: '桃微婚纱摄影--B套系',layout:'layout_pc' });
 }
 function index_c(req, res, next) {
-    res.render('pc/packagec', { title: '桃微婚纱摄影--C套系',layout:'layout_pc' });
+    res.render('pc/package/packagec', { title: '桃微婚纱摄影--C套系',layout:'layout_pc' });
 }
 function index_d(req, res, next) {
-    res.render('pc/packaged', { title: '桃微婚纱摄影--D套系',layout:'layout_pc' });
+    res.render('pc/package/packaged', { title: '桃微婚纱摄影--D套系',layout:'layout_pc' });
 }
 function index_e(req, res, next) {
-    res.render('pc/packagee', { title: '桃微婚纱摄影--E套系',layout:'layout_pc' });
+    res.render('pc/package/packagee', { title: '桃微婚纱摄影--E套系',layout:'layout_pc' });
 }
 module.exports = router;

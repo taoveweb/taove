ce3df0fa89dc35eda2a4cca06eab0395f389065b
taoveweb <taoveweb@gmail.com>
2015-11-11 17:16:33 +0800

    photoframes

--- routes/pc/photoframes.js
+++ routes/pc/photoframes.js
@@ -4,9 +4,13 @@
 var express = require('express');
 var router = express.Router();
 router.get('/', index);
-
+router.get('/detail', detail);
 function index(req, res, next) {
-    res.render('pc/photoframes', { title: '婚件产品',layout:'layout_pc' });
+    res.render('pc/photoframes/index', { title: '婚件产品',layout:'layout_pc' });
+}
+
+function detail(req, res, next) {
+    res.render('pc/photoframes/photoframes_detail', { title: '婚件产品详情',layout:'layout_pc' });
 }
 
 module.exports = router;

52eadde5a2ae62ac64a9053dd062a96346a018a2
taoveweb <taoveweb@gmail.com>
2015-12-25 09:48:23 +0800

    all

--- app.js
+++ app.js
@@ -4,37 +4,37 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session=require('express-session');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 var minify = require('express-minify');
-var fs=require('fs');
-var hbs=require('hbs');
+var fs = require('fs');
+var hbs = require('hbs');
 var compression = require('compression');
-global.__baseDir=__dirname;
+global.__baseDir = __dirname;
 
 var blocks = {};
 
 var app = express();
-app.locals.static ='/';
+app.locals.static = '/';
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
 
-
 /*
-hbs.registerPartial('partial', fs.readFileSync(__dirname + '/views/admin/admincss.hbs', 'utf8'));
-*/
+ hbs.registerPartial('partial', fs.readFileSync(__dirname + '/views/admin/admincss.hbs', 'utf8'));
+ */
 hbs.registerPartial('left', fs.readFileSync(__dirname + '/views/admin/left.hbs', 'utf8'));//????
 hbs.registerPartials(__dirname + '/views/partials');
-hbs.registerHelper('extend', function(name, context) {
-  var block = blocks[name];
-  if (!block) {
-    block = blocks[name] = [];
-  }
-  block.push(context.fn(this)); // for older versions of handlebars, use block.push(context(this));
+hbs.registerHelper('extend', function (name, context) {
+    var block = blocks[name];
+    if (!block) {
+        block = blocks[name] = [];
+    }
+    block.push(context.fn(this)); // for older versions of handlebars, use block.push(context(this));
 });
-hbs.registerHelper('block', function(name) {
-  var val = (blocks[name] || []).join('\n');
-  blocks[name] = [];
-  return val;
+hbs.registerHelper('block', function (name) {
+    var val = (blocks[name] || []).join('\n');
+    blocks[name] = [];
+    return val;
 });
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -44,13 +44,16 @@ app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  rolling:true,
-  cookie: { maxAge: 1000*60*30 },
-  saveUninitialized: true
+    secret: 'keyboard cat',
+    resave: false,
+    rolling: true,
+    cookie: {maxAge: 1000 * 60 * 30},
+    saveUninitialized: true,
+    store: new MongoStore({   //????mongodb???
+        url:'mongodb://taove:taove@localhost/test'
+    })
 }));
 app.use(cookieParser());
 app.use(compression());
@@ -63,10 +66,10 @@ require('./routes/route')(app);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -74,23 +77,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to buyer
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 

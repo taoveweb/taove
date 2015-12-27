25cd0ff2d284b8e07a4d40332c0e2145b3ead906
黄 家金 <hjj245148>
2015-12-28 07:26:46 +0800

    all"

--- public/admin/js/production.js
+++ public/admin/js/production.js
@@ -10,4 +10,37 @@ $(function(){
     $('.close').click(function(){
         $(this).parents('.pop').fadeOut();
     })
+
+
 })
+
+
+
+
+
+
+
+var manualUploader = new qq.FineUploader({
+    element: document.getElementById('fine-uploader-manual-trigger'),
+    template: 'qq-template-manual-trigger',
+    request: {
+        endpoint: '/admin/production'
+    },
+    thumbnails: {
+        placeholders: {
+            waitingPath: '/source/placeholders/waiting-generic.png',
+            notAvailablePath: '/source/placeholders/not_available-generic.png'
+        }
+    },
+    validation: {
+        allowedExtensions: ['jpeg', 'jpg', 'png'],
+        itemLimit: 5,
+        sizeLimit: 15000000
+    },
+    autoUpload: false,
+    debug: true
+});
+
+qq(document.getElementById("trigger-upload")).attach("click", function() {
+    manualUploader.uploadStoredFiles();
+});

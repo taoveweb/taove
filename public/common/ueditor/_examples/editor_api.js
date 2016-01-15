0348b3debdee802be6224239490592c6bc6bd433
taoveweb <taoveweb@gmail.com>
2016-01-15 14:44:32 +0800

    all

--- public/common/ueditor/_examples/editor_api.js
+++ public/common/ueditor/_examples/editor_api.js
@@ -122,7 +122,7 @@
             'adapter/autosave.js'
 
         ],
-        baseURL = '../_src/';
+        baseURL = '/common/ueditor/_src/';
     for (var i=0,pi;pi = paths[i++];) {
         document.write('<script type="text/javascript" src="'+ baseURL + pi +'"></script>');
     }

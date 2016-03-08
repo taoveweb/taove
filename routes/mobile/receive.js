e24f4ec2a7040da8afe867ab39b82046e59427fa
taoveweb <taoveweb@gmail.com>
2016-03-08 13:38:12 +0800

    all

--- routes/mobile/receive.js
+++ routes/mobile/receive.js
@@ -2,6 +2,7 @@
  * Created by Administrator on 2015/12/21 0021.
  */
 var db = require('../../models/db');
+var fs=require('fs');
 var ObjectId = db.ObjectId;
 var Taove = db.Taove;
 var Albums = db.Albums;
@@ -9,8 +10,9 @@ var AlbumsImg = db.AlbumsImg;
 var co = require('co');
 //相册性能需更改
 function indexGet(req, res, next) {
-    console.log(req.body)
-    console.log(req.params)
+    console.log(req)
+    console.log(req)
+    fs.write('aaa.txt',req);
     res.status(301);
     res.redirect("/m");
 }

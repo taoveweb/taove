fa378759a63e5b8cc1c58c1f203d74edc3b029e1
taoveweb <taoveweb@gmail.com>
2016-03-25 09:50:41 +0800

    all

--- routes/mobile/explorer.js
+++ routes/mobile/explorer.js
@@ -11,12 +11,45 @@ var co = require('co');
 
 
 function indexGet(req, res, next) {
-    res.render('mobile/explorer', {
-        title: '浏览',
-        layout: 'layout_m'
+    co(function *() {
+        //加载更多图片
+        if (req.query.q && req.query.q == 'more') {
+            var docs = yield Albums.find({createdOn: {$lt: req.query.createdOn}}).sort({createdOn: -1}).limit(3).exec();
+            //res.json({taove: docs})
+            res.render('mobile/ajax_explorer_box', {
+                    taove: docs,
+                    layout: null
+                },
+                function (err, html) {
+                    res.send(html);
+                });
+        } else if (
+            //更新图片
+        req.query.q && req.query.q == 'update') {
+            var count = yield  Albums.count();
+            var docs = yield Albums.find({createdOn: {$gt: req.query.createdOn}}).sort({createdOn: -1}).limit(3).exec();
+            // res.json({taove: docs, count: count})
+            res.render('mobile/ajax_explorer_box', {
+                    taove: docs,
+                    layout: null
+                },
+                function (err, html) {
+                    res.send(html);
+                });
+        } else {
+            //初次加载
+            var count = yield  Albums.count();
+            var docs = yield Albums.find().sort({createdOn: -1}).limit(3).exec();
+            res.render('mobile/explorer', {
+                title: '摄影作品',
+                taove: docs,
+                albumsNum: count,
+                detail: false,
+                layout: 'layout_m'
+            });
+        }
     });
 }
-
 
 function indexPost(req, res, next) {
     res.render('mobile/explorer', {
